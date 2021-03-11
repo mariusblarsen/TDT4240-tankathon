53daca7560de5d61bf1c72cc0f779dda8131747e
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-11 09:46:12 +0100

    libgdx setup

---
+++ desktop/src/tdt4240/tankathon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tdt4240.tankathon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tdt4240.tankathon.game.TankathonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TankathonGame(), config);
+	}
+}
