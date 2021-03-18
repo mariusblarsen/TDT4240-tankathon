329526e8cb4be9e21539861b9919072890785cc5
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-18 14:18:21 +0100

    added component mapper

--- core/src/tdt4240/tankathon/game/ecs/component/HealthComponent.kt
+++ core/src/tdt4240/tankathon/game/ecs/component/HealthComponent.kt
@@ -1,13 +1,23 @@
 package tdt4240.tankathon.game.ecs.component
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.gdx.utils.Pool
+import ktx.ashley.mapperFor
 
 class HealthComponent: Component, Pool.Poolable {
     var maxHealth: Float = 1000.0f  // TODO: Find and set real value
     var health: Float = maxHealth
 
     override fun reset() {
-        TODO("Not yet implemented")
+        health = maxHealth
+    }
+
+    companion object {
+        /* Perfomance
+        * ktx's mapperFor uses hashes for direct lookup of
+        * entities, with complexity O(1) instead of O(n)
+        *  */
+        val mapper: ComponentMapper<HealthComponent> = mapperFor<HealthComponent>()
     }
 }
