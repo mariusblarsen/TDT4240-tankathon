c715f3314f8226e53dfb953eb7d1628979c5c645
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-18 10:34:26 +0100

    converted generated files to kotlin

---
+++ desktop/src/tdt4240/tankathon/game/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package tdt4240.tankathon.game.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import tdt4240.tankathon.game.TankathonGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(TankathonGame(), config)
+    }
+}
