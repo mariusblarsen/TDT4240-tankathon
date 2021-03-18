73ebdf34e4c9ae79f09f2d17e063e72b92f702f9
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-18 14:18:00 +0100

    add more components to player entity

--- core/src/tdt4240/tankathon/game/ecs/ECSengine.kt
+++ core/src/tdt4240/tankathon/game/ecs/ECSengine.kt
@@ -3,8 +3,10 @@ package tdt4240.tankathon.game.ecs
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.gdx.math.Vector2
+import tdt4240.tankathon.game.ecs.component.HealthComponent
 import tdt4240.tankathon.game.ecs.component.PhysicsComponent
 import tdt4240.tankathon.game.ecs.component.PlayerComponent
+import tdt4240.tankathon.game.ecs.component.SpriteComponent
 
 /* PooledEngine
 *
@@ -15,13 +17,21 @@ class ECSengine: PooledEngine() {
 
     fun createPlayer(spawnPosition: Vector2) {
         val entity: Entity = this.createEntity()
+        val position: Vector2 = spawnPosition
 
-        val playerComponent : PlayerComponent? = createComponent(PlayerComponent::class.java)
-        entity.add(playerComponent)
+        /* Add player component to player */
+        entity.add(createComponent(PlayerComponent::class.java))
 
         /* Add box2D physics to the player */
-        val physicsComponent : PhysicsComponent? = createComponent(PhysicsComponent::class.java)
-        entity.add(physicsComponent)
+        entity.add(createComponent(PhysicsComponent::class.java))
+
+        /* Add Health to player */
+        entity.add(createComponent(HealthComponent::class.java))
+
+        /* Add Movement to player */
+        // TODO: add MovementComponent
+        /* Add Sprite to player */
+        entity.add(createComponent(SpriteComponent::class.java))
 
     }
 }
