ac47662118d9d3a162af34ddc7223014ce41237f
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-18 11:10:03 +0100

    Component shell

---
+++ core/src/tdt4240/tankathon/game/ecs/component/AIComponent.kt
@@ -0,0 +1,10 @@
+package tdt4240.tankathon.game.ecs.component
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool
+
+class AIComponent: Component, Pool.Poolable {
+    override fun reset() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ core/src/tdt4240/tankathon/game/ecs/component/SpriteComponent.kt
@@ -0,0 +1,11 @@
+package tdt4240.tankathon.game.ecs.component
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool
+
+class SpriteComponent: Component, Pool.Poolable {
+
+    override fun reset() {
+        TODO("Not yet implemented")
+    }
+}
