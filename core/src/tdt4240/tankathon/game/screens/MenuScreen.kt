d38ba56ebd74e9d7ba55ea6caaf9302cc3162e2f
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-23 16:51:54 +0100

    Created empty MenuScreen

---
+++ core/src/tdt4240/tankathon/game/screens/MenuScreen.kt
@@ -0,0 +1,12 @@
+package tdt4240.tankathon.game.screens
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import ktx.app.KtxScreen
+import tdt4240.tankathon.game.TankathonGame
+
+class MenuScreen(val game: TankathonGame) : KtxScreen {
+    override fun render(delta: Float) {
+    }
+}
