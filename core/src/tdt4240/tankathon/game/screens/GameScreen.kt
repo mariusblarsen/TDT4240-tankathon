5b8b63f498ea33d9f66feb37be05745c26b37c44
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-23 19:00:05 +0100

    gamescreen now uses viewport

--- core/src/tdt4240/tankathon/game/screens/GameScreen.kt
+++ core/src/tdt4240/tankathon/game/screens/GameScreen.kt
@@ -4,24 +4,36 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.graphics.use
 import ktx.log.info
 import ktx.log.logger
 import tdt4240.tankathon.game.TankathonGame
+import tdt4240.tankathon.game.UNIT_SCALE
+import tdt4240.tankathon.game.V_HEIGHT
+import tdt4240.tankathon.game.V_WIDTH
 
 private val LOG = logger<GameScreen>()
 
 class GameScreen(game: TankathonGame) : AbstractScreen(game){
     private val texture = Texture("tank.png")
-    private val sprite = Sprite(texture)
+    private val sprite = Sprite(texture).apply {
+        setSize(this.texture.width * UNIT_SCALE, this.texture.height * UNIT_SCALE)
+    }
+    val gameViewport = FitViewport(V_WIDTH.toFloat(), V_HEIGHT.toFloat())
 
     override fun show() {
         LOG.info { "Game Screen" }
         sprite.setPosition(1f, 1f)
     }
 
+    override fun resize(width: Int, height: Int) {
+        gameViewport.update(width, height, true)
+    }
+
     override fun render(delta: Float) {
-        batch.use {
+        gameViewport.apply()
+        batch.use(gameViewport.camera.combined) {
             sprite.draw(it)
         }
     }
