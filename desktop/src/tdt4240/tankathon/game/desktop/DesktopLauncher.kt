c5f219d9aa7b4d27ced7f73801b21d6d9aea4271
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-23 18:59:09 +0100

    resized desktoplauncher

--- desktop/src/tdt4240/tankathon/game/desktop/DesktopLauncher.kt
+++ desktop/src/tdt4240/tankathon/game/desktop/DesktopLauncher.kt
@@ -3,11 +3,15 @@ package tdt4240.tankathon.game.desktop
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import tdt4240.tankathon.game.TankathonGame
+import tdt4240.tankathon.game.V_HEIGHT
+import tdt4240.tankathon.game.V_WIDTH
 
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
         val config = LwjglApplicationConfiguration()
+        config.height = V_HEIGHT*32
+        config.width = V_WIDTH*32
         LwjglApplication(TankathonGame(), config)
     }
 }
