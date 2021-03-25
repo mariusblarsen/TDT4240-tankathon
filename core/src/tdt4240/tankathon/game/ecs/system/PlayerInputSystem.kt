14b1e68ac1a684349357ff1e68341dfe14e1b02e
Marius Larsen <mariubla@stud.ntnu.no>
2021-03-25 13:17:00 +0100

    removed FacingComponent

--- core/src/tdt4240/tankathon/game/ecs/system/PlayerInputSystem.kt
+++ core/src/tdt4240/tankathon/game/ecs/system/PlayerInputSystem.kt
@@ -7,35 +7,27 @@ import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.ashley.allOf
 import ktx.ashley.get
-import tdt4240.tankathon.game.ecs.component.FacingComponent
 import tdt4240.tankathon.game.ecs.component.PlayerComponent
 import tdt4240.tankathon.game.ecs.component.TransformComponent
 
 class PlayerInputSystem(
         private val gameViewport: Viewport
 ) : IteratingSystem(
-        allOf(PlayerComponent::class, TransformComponent::class, FacingComponent::class).get()
+        allOf(PlayerComponent::class, TransformComponent::class).get()
 ){
     private val inputVec = Vector2()
     override fun processEntity(entity: Entity, deltaTime: Float) {
         val transform = entity[TransformComponent.mapper]
         require(transform != null){ "Entity |entity| must have a TransformComponent. entity=$entity"}
-        val facing = entity[FacingComponent.mapper]
-        require(facing != null){ "Entity |entity| must have a FacingComponent. entity=$entity"}
+        val player = entity[PlayerComponent.mapper]
+        require(player != null){ "Entity |entity| must have a PlayerComponent. entity=$entity"}
 
-        /* Handle input
-        * :inputVec: position of input touch
-        * set facing direction by subtracting position of tank
-        * from input vector
-        * */
+        /* Handle input */
         inputVec.x = Gdx.input.x.toFloat()
         inputVec.y = Gdx.input.y.toFloat()
 
         gameViewport.unproject(inputVec)
-        val diffX = inputVec.x - transform.position.x - transform.size.x*0.5f
-        val diffY = inputVec.y - transform.position.y - transform.size.y*0.5f
-
-        facing.direction = Vector2(diffX, diffY)
+        transform.setRotation(inputVec)
     }
 }
 
