89c3b94f39481286c1b3198b6ceef4045465ea4b
Marius Larsen <mariubla@stud.ntnu.no>
2021-04-10 19:01:15 +0200

    setTexture method in sprite component

--- core/src/tdt4240/tankathon/game/ecs/component/SpriteComponent.kt
+++ core/src/tdt4240/tankathon/game/ecs/component/SpriteComponent.kt
@@ -2,10 +2,14 @@ package tdt4240.tankathon.game.ecs.component
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.Pool
 import ktx.ashley.get
 import ktx.ashley.mapperFor
+import tdt4240.tankathon.game.UNIT_SCALE
 
 class SpriteComponent: Component, Pool.Poolable, Comparable<SpriteComponent> {
     var sprite: Sprite = Sprite()
@@ -23,6 +27,22 @@ class SpriteComponent: Component, Pool.Poolable, Comparable<SpriteComponent> {
          */
         return (sprite.color.a - other.sprite.color.a).toInt()
     }
+
+    fun setTexture(texture: Texture, origin: Vector2){
+        sprite.run{
+            setRegion(texture)
+            setSize(texture.width * UNIT_SCALE, texture.height* UNIT_SCALE)
+            setOrigin(origin.x, origin.y)
+        }
+    }
+    fun setTexture(texture: Texture, origin: Vector3){
+        sprite.run{
+            setRegion(texture)
+            setSize(texture.width * UNIT_SCALE, texture.height* UNIT_SCALE)
+            setOrigin(origin.x, origin.y)
+        }
+    }
+
     companion object {
         val mapper = mapperFor<SpriteComponent>()
     }
