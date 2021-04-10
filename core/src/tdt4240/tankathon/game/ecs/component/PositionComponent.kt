88890d37ecebb66911f74aff956046e284184dbb
Marius Larsen <mariubla@stud.ntnu.no>
2021-04-10 19:00:39 +0200

    added position component

---
+++ core/src/tdt4240/tankathon/game/ecs/component/PositionComponent.kt
@@ -0,0 +1,19 @@
+package tdt4240.tankathon.game.ecs.component
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.ComponentMapper
+import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.utils.Pool
+import ktx.ashley.mapperFor
+
+class PositionComponent : Component, Pool.Poolable{
+    var position = Vector3(0f,0f, 0f)
+
+    override fun reset() {
+        position.set(Vector3.Zero)
+    }
+
+    companion object{
+        val mapper:ComponentMapper<PositionComponent> = mapperFor<PositionComponent>()
+    }
+}
