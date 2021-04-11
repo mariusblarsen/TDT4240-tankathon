d53c6dc473bac22a5989f5a783399ad3e57fa767
btlerber <bjorn.lerberg@gmail.com>
2021-04-12 01:00:30 +0200

    first iteration of working menu

--- core/src/tdt4240/tankathon/game/screens/MenuScreen.kt
+++ core/src/tdt4240/tankathon/game/screens/MenuScreen.kt
@@ -2,13 +2,14 @@ package tdt4240.tankathon.game.screens
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import ktx.graphics.use
 import ktx.log.info
 import ktx.log.logger
@@ -18,90 +19,83 @@ import tdt4240.tankathon.game.TankathonGame
 private val LOG = logger<MenuScreen>()
 
 class MenuScreen(game: TankathonGame) : AbstractScreen(game) {
+    //TODO implementer object pool
     val font : BitmapFont = BitmapFont()
-    //vurder objectpool
-
-    var startButton : Rectangle
-    var settingsButton : Rectangle
-    var scoreboardButton : Rectangle
-    var buttonImage : Texture
-
     var touchPos : Vector3
-    var camera : OrthographicCamera
+    var skin : Skin = Skin()
+    var mainMenuStage : Stage  = Stage()
+    var buttonAtlas :  TextureAtlas
+
+    //buttons
+
+    lateinit var startTextButton : TextButton
+    lateinit var settingsTextButton : TextButton
+    lateinit var scoreboardTextButton : TextButton
+    lateinit var exitTextButton : TextButton
+
+    /*
+    lateinit var buttonStartImage : Texture
+    lateinit var buttonScoreImage : Texture
+    lateinit var buttonSettingsImage : Texture
+    */
 
     //størrelsen på knappene
-    val buttonHeight = 20f
+    val buttonHeight = 10f
     val buttonWidth = 200f
     val buttonSpace = 20f
 
-
+    //størrelse på skjerm
     val cameraMiddleWidth : Float
     val cameraMiddleHeight : Float
 
-
     override fun show() {
         LOG.info { "Menu Screen" }
-
     }
 
     init {
+        Gdx.input.setInputProcessor(mainMenuStage);
+
         //TODO: opretter nytt kamera med bredde fra objectpool, kan forandre kamera til object pool om det er ortografisk
-
-        camera = OrthographicCamera(480f, 270f)
-
-
-        cameraMiddleHeight = camera.viewportHeight/2f
-        cameraMiddleWidth = camera.viewportWidth/2f
-        println("camera height: " + cameraMiddleHeight.toString() + " width: " + cameraMiddleWidth.toString())
-
-        //TODO: bytte ut rektangel med button
-        startButton = Rectangle(
-                cameraMiddleWidth - buttonWidth / 2f,
-                cameraMiddleHeight + buttonHeight / 2f + buttonSpace,
-                buttonWidth,
-                buttonHeight)
-        scoreboardButton = Rectangle(
-                cameraMiddleWidth - buttonWidth / 2f,
-                cameraMiddleHeight + buttonHeight / 2f,
-                buttonWidth,
-                buttonHeight)
-        settingsButton = Rectangle(
-                cameraMiddleWidth - buttonWidth / 2f,
-                cameraMiddleHeight - buttonHeight / 2f - buttonSpace,
-                buttonWidth,
-                buttonHeight)
-
-        //create buttons
-
-        // temporary until we have asset manager in
-
-        // temporary until we have asset manager in
+        cameraMiddleWidth = Gdx.graphics.height.toFloat()/2f
+        cameraMiddleHeight = Gdx.graphics.width.toFloat()/2f
+        //initRectangles()
 
 
+        buttonAtlas = TextureAtlas(Gdx.files.internal("Neon_UI_Skin/neonui/neon-ui.atlas"));
+        skin.addRegions(buttonAtlas)
+        skin.load(Gdx.files.internal("Neon_UI_Skin/neonui/neon-ui.json"))
 
+        initButtons()
 
-        //touch/mus
         touchPos = Vector3()
 
-        //button bilde
-        buttonImage = Texture(Gdx.files.internal("/Users/Bjorn/progArki/TDT4240-tankathon/core/src/tdt4240/tankathon/game/screens/button.png"))
+        /*
+        var startImage = Texture(Gdx.files.internal("buttonStart.png"))
+        var SettingsImage = Texture(Gdx.files.internal("buttonSettings.png"))
+        var highscoreImage = Texture(Gdx.files.internal("buttonScore.png"))
+        */
+
+
+
+
     }
 
     override fun render(delta: Float) {
-        //TODO: gjøre så kameraet også skalerer etter skjerm.
-
-        //batch.begin()
         //tømmer skjerm og setter bakgrunn
         Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        //oppdater kamera en gang per frame
-        camera.update()
+
+        //kan slettes tror eg
+        update(delta)
+
+        mainMenuStage.draw()
+
+
 
         batch.use {
-            font.draw(it, "Menu\nPress any key to start game" + cameraMiddleWidth, 50f, 280f)
+            //gammel
+            //drawRectangles()
 
-            batch.draw(buttonImage, startButton.x, startButton.y, startButton.width, startButton.height)
-            font.draw(it, "Menu\nPress any key to start game" + cameraMiddleWidth, 50f, 280f)
 
         }
 
@@ -111,22 +105,91 @@ class MenuScreen(game: TankathonGame) : AbstractScreen(game) {
             //camera.unproject(touchPos)
             //bucket.x = touchPos.x - 64f/2f
         }
-
-
-
-
-
-
-
-
-
         /*
-        if(Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY) || Gdx.input.isTouched){
+        if(Gdx.input.isKeyJustPressed(Input.) || Gdx.input.isTouched){
             game.setScreen<GameScreen>()
         }
-
          */
     }
+
+
+    fun initButtons(){
+        var startTextButton = TextButton("start", skin)
+        var scoreboardTextButton = TextButton("ScoreBoard", skin)
+        var settingsTextButton = TextButton("settings", skin)
+        var exitTextButton = TextButton("exit", skin)
+
+
+
+        startTextButton.setPosition(cameraMiddleWidth, (cameraMiddleHeight) + buttonHeight)
+        scoreboardTextButton.setPosition(startTextButton.x, startTextButton.y - buttonSpace - buttonHeight)
+        settingsTextButton.setPosition(startTextButton.x, scoreboardTextButton.y - buttonSpace - buttonHeight)
+        exitTextButton.setPosition(startTextButton.x, scoreboardTextButton.y - buttonSpace - buttonHeight - buttonSpace - buttonHeight)
+
+        startTextButton.setSize(buttonWidth, buttonHeight)
+        scoreboardTextButton.setSize(buttonWidth, buttonHeight)
+        settingsTextButton.setSize(buttonWidth, buttonHeight)
+        exitTextButton.setSize(buttonWidth, buttonHeight)
+
+
+
+
+        startTextButton.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent?, actor: Actor?) {
+                game.setScreen<GameScreen>()
+            }
+        })
+        exitTextButton.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent?, actor: Actor?) {
+                Gdx.app.exit()
+            }
+        })
+
+        mainMenuStage.addActor(startTextButton)
+        mainMenuStage.addActor(scoreboardTextButton)
+        mainMenuStage.addActor(settingsTextButton)
+        mainMenuStage.addActor((exitTextButton))
+    }
+
+/*
+    fun initRectangles(){
+        startButton = Rectangle(
+                (cameraMiddleWidth - buttonWidth/2f),
+                (cameraMiddleHeight - buttonHeight/2f) + buttonSpace + buttonHeight,
+                buttonWidth,
+                buttonHeight)
+        scoreboardButton = Rectangle(
+                startButton.x,
+                startButton.y - buttonSpace - buttonHeight,
+                buttonWidth,
+                buttonHeight)
+        settingsButton = Rectangle(
+                startButton.x,
+                scoreboardButton.y - buttonSpace - buttonHeight,
+                buttonWidth,
+                buttonHeight)
+        buttonStartImage = Texture(Gdx.files.internal("buttonStart.png"))
+        buttonScoreImage = Texture(Gdx.files.internal("buttonScore.png"))
+        buttonSettingsImage = Texture(Gdx.files.internal("buttonSettings.png"))
+    }
+
+    fun drawRectangles() {
+        font.draw(batch, "Menu", cameraMiddleWidth - buttonWidth / 2f, startButton.y + buttonHeight + buttonSpace * 2f)
+        //start
+        batch.draw(buttonStartImage, startButton.x, startButton.y, startButton.width, startButton.height)
+        font.draw(batch, "start", startButton.x, startButton.y)
+        //scoreboard
+        batch.draw(buttonScoreImage, scoreboardButton.x, scoreboardButton.y, scoreboardButton.width, scoreboardButton.height)
+        font.draw(batch, "scoreboard", scoreboardButton.x, scoreboardButton.y)
+        //settings
+        batch.draw(buttonSettingsImage, settingsButton.x, settingsButton.y, settingsButton.width, settingsButton.height)
+        font.draw(batch, "settings", settingsButton.x, settingsButton.y)
+    }
+*/
+    fun update(delta: Float) {
+    mainMenuStage.act(delta)
+    }
+
     override fun resize(width: Int, height: Int) { }
     override fun hide() { }
     override fun pause() { }
@@ -134,7 +197,13 @@ class MenuScreen(game: TankathonGame) : AbstractScreen(game) {
 
 
     override fun dispose() {
-        buttonImage.dispose()
+    //TODO implementer kasting av alle knapper osv
+        /*
+        buttonStartImage.dispose()
+        buttonScoreImage.dispose()
+        buttonSettingsImage.dispose()
+         */
+
     }
 
 }
