73a9d1a7cb15ec45f85437a692082b6ebccdc013
Marius Larsen <mariubla@stud.ntnu.no>
2021-04-13 18:52:24 +0200

    added removesystem

---
+++ core/src/tdt4240/tankathon/game/ecs/system/RemoveSystem.kt
@@ -0,0 +1,16 @@
+package tdt4240.tankathon.game.ecs.system
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.systems.IteratingSystem
+import ktx.ashley.allOf
+import ktx.ashley.get
+import tdt4240.tankathon.game.ecs.component.RemoveComponent
+
+class RemoveSystem() : IteratingSystem(allOf(RemoveComponent::class).get()) {
+    override fun processEntity(entity: Entity, deltaTime: Float) {
+        val remove = entity[RemoveComponent.mapper]
+        require(remove != null) { "Entity |entity| must have a RemoveComponent. entity=$entity" }
+
+        engine.removeEntity(entity)
+    }
+}
