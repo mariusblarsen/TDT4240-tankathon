19888d06dda3471b2993c146de078a6ebdc9adc7
Marius Larsen <mariubla@stud.ntnu.no>
2021-04-16 10:24:51 +0200

    checkes through all objects before moving

--- core/src/tdt4240/tankathon/game/ecs/system/MovementSystem.kt
+++ core/src/tdt4240/tankathon/game/ecs/system/MovementSystem.kt
@@ -25,6 +25,15 @@ class MovementSystem(private val ecSengine: ECSengine) : IteratingSystem(
         ecSengine.getEntitiesFor(allOf(MapObjectComponent::class).get())
     }
     private val threshold: Float = 0.1f
+    var collisionOnRight: Boolean = false
+    var collisionOnLeft : Boolean = false
+    var movingRight: Boolean = false
+    var collisionUpwards: Boolean = false
+    var collisionDownwards: Boolean = false
+    var movingUp: Boolean = false
+    var horizontalCollision: Boolean = false
+    var verticalCollision: Boolean = false
+
     override fun processEntity(entity: Entity, deltaTime: Float) {
         val position = entity[PositionComponent.mapper]
         require(position != null){ "Entity |entity| must have a PositionComponent. entity=$entity"}
@@ -37,15 +46,12 @@ class MovementSystem(private val ecSengine: ECSengine) : IteratingSystem(
                 position.position.x,
                 position.position.y,
                 physicsComponent.width,
-                physicsComponent.height)
+                physicsComponent.height
+        )
+        movingRight = velocity.direction.x > 0
+        movingUp = velocity.direction.y > 0
 
-        var collisionOnRight: Boolean
-        var collisionOnLeft : Boolean
-        var movingRight: Boolean
-        var collisionUpwards: Boolean
-        var collisionDownwards: Boolean
-        var movingUp: Boolean
-        var horizontalCollision: Boolean
+
         mapObjects.forEach{ objectEntity ->
             objectEntity[MapObjectComponent.mapper]?.let { mapComponent ->
                 val obstacle = mapComponent.hitbox
@@ -54,32 +60,45 @@ class MovementSystem(private val ecSengine: ECSengine) : IteratingSystem(
                     if(entity.contains(BulletComponent.mapper)){
                         entity.addComponent<RemoveComponent>(ecSengine)
                     }
-                    movingRight = velocity.direction.x > 0
-                    collisionOnRight = movingHitbox.x < obstacle.x
-                    collisionOnLeft = movingHitbox.x > obstacle.x + obstacle.width - threshold
 
-                    movingUp = velocity.direction.y > 0
-                    collisionUpwards = movingHitbox.y < obstacle.y + threshold
-                    collisionDownwards = movingHitbox.y + threshold > obstacle.y + obstacle.height
+                    collisionOnRight = (movingHitbox.x < obstacle.x) || collisionOnRight
+                    collisionOnLeft = (movingHitbox.x > obstacle.x + obstacle.width - threshold) ||collisionOnLeft
 
-                    horizontalCollision = (movingHitbox.y > obstacle.y + threshold) && (movingHitbox.y < obstacle.y + obstacle.height - threshold)
-                    if (horizontalCollision){
-                        position.position.y+=velocity.direction.y*velocity.speed*deltaTime
-                        if ((movingRight and collisionOnLeft)
-                                or (!movingRight and collisionOnRight)){
-                            position.position.x+=velocity.direction.x*velocity.speed*deltaTime
-                        }
-                    } else {
-                        position.position.x+=velocity.direction.x*velocity.speed*deltaTime
-                        if ((movingUp and collisionDownwards) or (!movingUp and collisionUpwards)) {
-                            position.position.y += velocity.direction.y * velocity.speed * deltaTime
-                        }
-                    }
-                    return
+                    collisionUpwards = (movingHitbox.y < obstacle.y + threshold) ||collisionUpwards
+                    collisionDownwards = (movingHitbox.y + threshold > obstacle.y + obstacle.height) ||collisionDownwards
+
+                    horizontalCollision = ((movingHitbox.y > obstacle.y + threshold)
+                            && (movingHitbox.y < obstacle.y + obstacle.height - threshold))
+                            || horizontalCollision
+                    verticalCollision = ((movingHitbox.x > obstacle.x + threshold)
+                            && (movingHitbox.x < obstacle.x + obstacle.width - threshold))
+                            || verticalCollision
                 }
             }
         }
-        position.position.x+=velocity.direction.x*velocity.speed*deltaTime
-        position.position.y+=velocity.direction.y*velocity.speed*deltaTime
+        if (horizontalCollision){
+            if (!verticalCollision){
+                position.position.y+=velocity.direction.y*velocity.speed*deltaTime
+            }
+            if ((movingRight and collisionOnLeft)
+                    or (!movingRight and collisionOnRight)){
+                position.position.x+=velocity.direction.x*velocity.speed*deltaTime
+            }
+        } else if (verticalCollision){
+            position.position.x+=velocity.direction.x*velocity.speed*deltaTime
+            if ((movingUp and collisionDownwards) or (!movingUp and collisionUpwards)) {
+                position.position.y += velocity.direction.y * velocity.speed * deltaTime
+            }
+        } else {
+            position.position.x+=velocity.direction.x*velocity.speed*deltaTime
+            position.position.y+=velocity.direction.y*velocity.speed*deltaTime
+        }
+
+        collisionOnRight = false
+        collisionOnLeft = false
+        collisionUpwards = false
+        collisionDownwards = false
+        horizontalCollision = false
+        verticalCollision = false
     }
 }
